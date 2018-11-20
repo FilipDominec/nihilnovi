fedc5c9fc1b432a302d9c97273963fc2a54aa0bb
Filip Dominec <filip.dominec@gmail.com>
2018-11-20 19:30:59 +0100

    reformat of snippet

--- my_snippets/analysis/curve_fit.py
+++ my_snippets/analysis/curve_fit.py
@@ -4,8 +4,8 @@
     #           p0:     initial values of parameters
     #           Gauss(): function to be fitted with, rename & edit it to your needs
     # outputs:  popt:   parameters of the best fit
-    def Gauss(x, A, x0, FWHM): return A*np.exp(-(x-x0)**2/FWHM**2 *4*np.log(2))
-    p0 = (.01, 450,20)      
+    def Gauss(x, A, x0, FWHM): return A * np.exp(-(x-x0)**2 / (FWHM/2)**2 * np.log(2))
+    p0 = (.01, 450, 20)      
     from scipy.optimize import curve_fit
     popt, pcov = curve_fit(Gauss, x, y, p0)
     print(popt, label)
