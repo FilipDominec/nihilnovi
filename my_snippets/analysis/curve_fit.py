62b477a8b7f224519309da2ff07b773a6023993f
Filip Dominec <filip.dominec@gmail.com>
2019-04-02 17:22:56 +0200

    renaming to generic fitf in example

--- my_snippets/analysis/curve_fit.py
+++ my_snippets/analysis/curve_fit.py
@@ -2,12 +2,12 @@
     # Simple data fitting by a given function
     # inputs:   x,y:    usual numpy arrays with coordinates for single curve
     #           p0:     initial values of parameters
-    #           Gauss(): function to be fitted with, rename & edit it to your needs
+    #           fitf(): function to be fitted with: a Gaussian by default, but edit it to your needs
     # outputs:  popt:   parameters of the best fit
-    def Gauss(x, A, x0, FWHM): return A * np.exp(-(x-x0)**2 / (FWHM/2)**2 * np.log(2))
+    def fitf(x, A, x0, FWHM): return A * np.exp(-(x-x0)**2 / (FWHM/2)**2 * np.log(2))
     p0 = (.01, 450, 20)      
     from scipy.optimize import curve_fit
-    popt, pcov = curve_fit(Gauss, x, y, p0)
+    popt, pcov = curve_fit(fitf, x, y, p0)
     print(popt, label)
-    ax.plot(x, Gauss(x, *popt), c='k')  # plots the fitted curve
+    ax.plot(x, fitf(x, *popt), color='k', linestyle='--', linewidth=0.5)  # plots the fitted curve
 
