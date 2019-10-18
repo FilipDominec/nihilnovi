2bdab56104b37d75f5be67dd4ba3af7eed721d27
Filip Dominec <filip.dominec@gmail.com>
2019-10-18 13:11:18 +0200

    testing the mpl gui

--- plotcommander.py
+++ plotcommander.py
@@ -16,8 +16,12 @@ from gi.repository.GdkPixbuf import Pixbuf, Colorspace
 
 import matplotlib
 import matplotlib.cm as cm
-from matplotlib.backends.backend_gtk3cairo import FigureCanvasGTK3Cairo  as FigureCanvas # "..Agg" backend is broken currently
+#from matplotlib.backends.backend_gtk3cairo import FigureCanvasGTK3Cairo  as FigureCanvas # "..Agg" backend is broken currently
+#from matplotlib.backends.backend_gtk3cairo import NavigationToolbar2GTK3 as NavigationToolbar# "..Agg" backend is broken currently
+from matplotlib.backends.backend_gtk3      import FigureCanvasGTK3 as FigureCanvas  # if not specified, Python3 freezes
 from matplotlib.backends.backend_gtk3      import NavigationToolbar2GTK3 as NavigationToolbar # if not specified, Python3 freezes
+#from matplotlib.backends.backend_tkagg import FigureCanvasTk as FigureCanvas
+#from matplotlib.backends.backend_tkagg import NavigationToolbar2Tk as NavigationToolbar
 #from mpl_toolkits.axes_grid1 import host_subplot ## allows better axes handling than fig.subplot
 from matplotlib.widgets import Cursor
 
