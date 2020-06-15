5b7bd76bb021e3af9cb439294693bd9ef73a6bf8
Filip Dominec <filip.dominec@gmail.com>
2020-06-15 22:38:53 +0200

    snippets cleanup

--- my_snippets/analysis/curve_fit.py
+++ my_snippets/analysis/curve_fit.py
@@ -4,10 +4,15 @@
     #           p0:     initial values of parameters
     #           fitf(): function to be fitted with: a Gaussian by default, but edit it to your needs
     # outputs:  popt:   parameters of the best fit
-    def fitf(x, A, x0, FWHM): return A * np.exp(-(x-x0)**2 / (FWHM/2)**2 * np.log(2))
-    p0 = (.01, 450, 20)      
+    #           stored_popt:   all fit parameters (in case of multiple curves, they may make an interesting
+    #                          function of the curves parameter 'param')
+    def fitf(x,    A,   x0,  FWHM): return A * np.exp(-(x-x0)**2 / (FWHM/2)**2 * np.log(2))
+    p0 =          (.01, 450, 20  )      
     from scipy.optimize import curve_fit
     popt, pcov = curve_fit(fitf, x, y, p0)
-    print(popt, label)
     ax.plot(x, fitf(x, *popt), color='k', linestyle='--', linewidth=0.5)  # plots the fitted curve
+    stored_popt = np.array([param]+popt) if 'stored_popt' not in locals() else np.vstack((stored_popt.T, [param]+popt)).T
+    import inspect; np.savetxt('fitted_parameters.dat', np.vstack([stored_popt]).T, header='\t'.join(['param']+inspect.getargspec(fitf).args[1:])) 
+
+    
 
