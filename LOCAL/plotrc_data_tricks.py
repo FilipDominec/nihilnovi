8ccdb5482415c813528d40b55dfd190ae2bc8a7e
Filip Dominec <filip.dominec@gmail.com>
2020-09-29 18:33:15 +0200

    data tricks start

---
+++ LOCAL/plotrc_data_tricks.py
@@ -0,0 +1,6 @@
+    #  * Data Tricks Contents
+    #           horizontal clipping         vertical clipping               convolution (moving average)        interpolation to a new axis
+    #           simple function fit         errorbar-aware fit              differential evolution fitting      covariance ellipse
+    #           1D/2D singular value decomp FFT/iFFT spectral filtering     smooth dataset joining              user interpolated function  
+
+
