11370aae8cc5d85e5a150a47755a6c73ec5af6c8
Filip Dominec <filip.dominec@gmail.com>
2020-10-25 19:21:08 +0100

    universal object browser fix

--- explore-obj/ex.py
+++ explore-obj/ex.py
@@ -2,48 +2,52 @@
 #-*- coding: utf-8 -*-
 
 import liborigin
-opj = liborigin.parseOriginFile('x.opj')
+import sys
+try:
+    opj = liborigin.parseOriginFile(sys.argv[1])
+except IndexError:
+    print('Please specify the origin file'); quit()
 
-coloured = False
+coloured = 0 #$False
 if coloured:
     normal = "\033[1;0m"
     bold   = "\033[1;1m"
-    grey   = "\033[1;2m"
+    grey   = "\033[2;2m"
 else:
     normal, bold  , grey   = '', '', ''
 maxlistlen = 5
 allstrings = []
 def explore(obj, parentstr, indent):
-    print(indent + parentstr)
+    print(indent + parentstr, end='')
     if coloured: indent = indent + grey + parentstr + normal ## prints ancestry objects with grey color for better clarity (terminal only)
-    else:        indent = indent + ' '*len(parentstr) ## replaces ancestry objects with whitespace for clarity
+    else:        indent = indent + '_'*len(parentstr) ## replaces ancestry objects with whitespace for clarity
     if len(indent)>200: 
         print('                          --------------- nesting too deep - halted here ----------------')
         return
 
     if type(obj) == list:
-        print(indent+bold+'[', normal)
+        print(bold+'[', normal)
         for n, item in enumerate(obj[:maxlistlen]):
-            explore(item, '[%d]' % n,  indent)
+            explore(item, 'item #%d' % n,  indent)
         if len(obj) > maxlistlen:
             print(indent+bold+'--- listing truncated here, %d values would follow ---' % (len(obj)-maxlistlen))
-        print(indent+bold+']', normal)
+        print(bold+']', normal)
     elif type(obj) == dict:
-        print(indent+bold+'{', normal)
+        print(bold+'{', normal)
         for key, value in obj.items():
             explore(value, '["'+key+'"]', indent)
-        print(indent+bold+'}', normal)
+        print(bold+'}', normal)
     elif type(obj) == str:
-        print(indent+bold+'"'+obj+'"', normal)
+        print(bold+'"'+obj+'"', normal)
         allstrings.append(obj)
     elif type(obj) == bytes:
-        print(indent+bold+' b"'+obj.decode('utf-8').strip()+'"')
+        print(bold+' b"'+obj.decode('utf-8').strip()+'"')
     elif type(obj) in (float, int, bool):
-        print(indent+bold,obj, normal)
+        print(bold,obj, normal)
     else:
-        print(indent, bold, obj, grey, type(obj), normal)
+        print(bold, obj, grey, type(obj), normal)
         usefulattrs = [attrname for attrname in dir(obj) if not attrname.startswith('__')]
-        print(' '*(len(indent)-len(parentstr)), grey, usefulattrs, normal)
+        #print(' '*(len(indent)-len(parentstr)), grey, usefulattrs, normal)
         for attrname in usefulattrs:
             explore(getattr(obj, attrname), '.'+attrname, indent)
     
