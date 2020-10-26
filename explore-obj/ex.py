4528131054a5ad2ea7ab9c44a6118946e2cd4c52
Filip Dominec <filip.dominec@gmail.com>
2020-10-26 11:43:34 +0100

    tree-like print

--- explore-obj/ex.py
+++ explore-obj/ex.py
@@ -8,19 +8,24 @@ try:
 except IndexError:
     print('Please specify the origin file'); quit()
 
-coloured = 0 #$False
-if coloured:
-    normal = "\033[1;0m"
-    bold   = "\033[1;1m"
-    grey   = "\033[2;2m"
+style = 'x' #$False
+if style == 'color':
+    normal = r"\033[1;0m"
+    bold   = r"\033[1;1m"
+    grey   = r"\033[2;2m"
 else:
     normal, bold  , grey   = '', '', ''
 maxlistlen = 5
 allstrings = []
+
+branchdict = {'│':'├', '║':'╟', '┃':'┣'}
+
 def explore(obj, parentstr, indent):
-    print(indent + parentstr, end='')
-    if coloured: indent = indent + grey + parentstr + normal ## prints ancestry objects with grey color for better clarity (terminal only)
-    else:        indent = indent + '_'*len(parentstr) ## replaces ancestry objects with whitespace for clarity
+
+    print(indent[:-1]+ branchdict.get(indent[-1:],'') + parentstr, end='')
+
+    if style == 'color': indent = indent + grey + parentstr + normal ## prints ancestry objects with grey color for better clarity (terminal only)
+    else:        indent = indent + ' '*(len(parentstr)+1) ## replaces ancestry objects with whitespace for clarity
     if len(indent)>200: 
         print('                          --------------- nesting too deep - halted here ----------------')
         return
@@ -28,14 +33,14 @@ def explore(obj, parentstr, indent):
     if type(obj) == list:
         print(bold+'[', normal)
         for n, item in enumerate(obj[:maxlistlen]):
-            explore(item, 'item #%d' % n,  indent)
+            explore(item, 'item #%d' % n,  indent[:-1]+'║')
         if len(obj) > maxlistlen:
             print(indent+bold+'--- listing truncated here, %d values would follow ---' % (len(obj)-maxlistlen))
-        print(bold+']', normal)
+        print(indent, bold+']', normal)
     elif type(obj) == dict:
         print(bold+'{', normal)
         for key, value in obj.items():
-            explore(value, '["'+key+'"]', indent)
+            explore(value, '["'+key+'"]', indent[:-1]+'┃')
         print(bold+'}', normal)
     elif type(obj) == str:
         print(bold+'"'+obj+'"', normal)
@@ -49,7 +54,7 @@ def explore(obj, parentstr, indent):
         usefulattrs = [attrname for attrname in dir(obj) if not attrname.startswith('__')]
         #print(' '*(len(indent)-len(parentstr)), grey, usefulattrs, normal)
         for attrname in usefulattrs:
-            explore(getattr(obj, attrname), '.'+attrname, indent)
+            explore(getattr(obj, attrname), '.'+attrname, indent[:-1]+'│')
     
 explore(opj, '', '')
 print(allstrings)
