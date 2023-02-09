dc87d96dfe9422474b87a796941f628b801d20da
Filip Dominec <filip.dominec@gmail.com>
2023-02-09 13:15:28 +0100

    decimation snippet

---
+++ my_snippets/analysis/decimate_with_moving_average.py
@@ -0,0 +1,5 @@
+    ## Moving average -> near-lossless decimation (takes few ms)
+    subdiv = 5 # reduce point number by 5 
+    convol = np.ones(subdiv)/subdiv  # optionally, quasi-Gaussian smoothing 2**-np.linspace(-1,1,subdiv)**2
+    x = x[int(subdiv/2):-int(subdiv/2):subdiv] # + (E[1]-E[0])*subdiv/2 # requires x to be equidistant
+    y = np.convolve(y, convol/np.sum(convol), mode='valid')[::subdiv] 
