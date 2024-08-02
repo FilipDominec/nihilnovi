f5e7160ea55d904f1d425a81638f5cfee5be19dc
Filip Dominec <filip.dominec@protonmail.com>
2024-08-02 16:00:24 +0200

    arranging and extending py snippets

--- my_snippets/preprocessing/simple_smoothing.py
+++ my_snippets/preprocessing/simple_smoothing.py
@@ -1,3 +1,10 @@
-    WIDTH = 25 # points for (truncated-Gaussian) smoothing kernel
-    convol = 2**-np.linspace(-2,2,WIDTH)**2
-    y = np.convolve(y,convol/np.sum(convol), mode='same') ## simple smoothing
+    def convolve_without_edge_artifacts(ar, ker): 
+        # at edges, the curve won't be bent towards 0, but may become more noisy
+        norm = np.convolve(np.ones_like(ar), ker, mode='same')
+        return np.convolve(ar, ker, mode='same') / norm
+
+    smooth_points = 25 # Select width for the (truncated-Gaussian) smoothing kernel
+    smooth_kernel = 2**-np.linspace(-3,3,smooth_points)**2 # Or use any function you like
+    smooth_kernel /= np.sum(smooth_kernel) # Roughly maintain the curve height
+
+    y = convolve_without_edge_artifacts(y, smooth_kernel)
