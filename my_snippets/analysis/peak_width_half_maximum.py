5d06011158186a8ecfb1bfa9a3616d6c30c997ae
Filip Dominec <filip.dominec@gmail.com>
2024-08-22 12:44:01 +0200

    snippet update from practice

--- my_snippets/analysis/peak_width_half_maximum.py
+++ my_snippets/analysis/peak_width_half_maximum.py
@@ -1,8 +1,13 @@
 def half_max_x(x, y, half=0.5):
     """ returns two points at X: where the curve first rises above half maximum, and where it last drops below it """
+    def lin_interp(x, y, i, halfmax):
+        return x[i] + (x[i+1] - x[i]) * ((halfmax - y[i]) / (y[i+1] - y[i]))
     halfmax = max(y)*half
     signs = np.sign(np.add(y, -halfmax))
     zero_crossings = (signs[0:-2] != signs[1:-1])
     zero_crossings_i = np.where(zero_crossings)[0]
+    if len(zero_crossings_i) < 2: 
+       print("crossings not found")
+       return(np.NaN, np.NaN)
     return [lin_interp(x, y, zero_crossings_i[0], halfmax),
-            lin_interp(x, y, zero_crossings_i[1], halfmax)]
+            lin_interp(x, y, zero_crossings_i[-1], halfmax)]
--- my_snippets/annotation/secondary_axis_horizontal.py
+++ my_snippets/annotation/secondary_axis_horizontal.py
@@ -1,1 +1,11 @@
-TODO!
+
+ax_right = ax.twinx()
+# now you can independently use ax_right.plot() etc. to get independent, but overlapping graphs as usual in papers
+
+## Grouping the lines/datapoints can be elegantly done with unicode 
+## symbols:    21a9 ↩   and    21aa ↪     (or 21b2, 21b3 are larger alternatives)
+ax.text('↩', size=30)
+ax_right.text('↪', size=30)
+# TODO!
+
+
